f84cd58bd8d368d070a08a4263ef0806ebab2c36
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-30 20:07:06 -0300

    feat(index): add new link css

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-
+<link rel="stylesheet" href="src/styles/style.css">
 <body class="bg-dark">
 	<div class="col-md-6">
 		<h1>Faltam 155 dias para seu aniversário</h1>
