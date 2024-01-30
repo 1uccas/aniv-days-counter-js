8db94e17c16aae2adc8802e6c1268da57d840067
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-30 20:50:04 -0300

    feat(index): new div loading

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 <link rel="stylesheet" href="src/styles/style.css">
 <body class="bg-dark">
-	<div class="col-md-6">
+	<div class="master">
 		<h1>Faltam 155 dias para seu aniversário</h1>
+		<!--LOADING HAMSTER-->
+
+		<div aria-label="Orange and tan hamster running in a metal wheel" role="img" class="wheel-and-hamster">
+		<div class="wheel"></div>
+		<div class="hamster">
+			<div class="hamster__body">
+				<div class="hamster__head">
+					<div class="hamster__ear"></div>
+					<div class="hamster__eye"></div>
+					<div class="hamster__nose"></div>
+				</div>
+				<div class="hamster__limb hamster__limb--fr"></div>
+				<div class="hamster__limb hamster__limb--fl"></div>
+				<div class="hamster__limb hamster__limb--br"></div>
+				<div class="hamster__limb hamster__limb--bl"></div>
+				<div class="hamster__tail"></div>
+			</div>
+		</div>
+		<div class="spoke"></div>
+	</div>
 	</div>
 </body>
