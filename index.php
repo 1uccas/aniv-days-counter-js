5991f60ebfd9b9d020233e124e80af05e3a14840
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-30 20:05:03 -0300

    feat(index): add new class and tag h1

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
 <body class="bg-dark">
-	
+	<div class="col-md-6">
+		<h1>Faltam 155 dias para seu aniversário</h1>
+	</div>
 </body>
