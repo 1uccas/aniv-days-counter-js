e76e864d770b228f9712ebc11052d9b50b84a65e
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-30 21:34:30 -0300

    feat(index): add function returnDays(html)

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
+<?php 
+include 'src/php/counter_days.php';
+?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 <link rel="stylesheet" href="src/styles/style.css">
 <body class="bg-dark">
 	<div class="master">
-		<h1>Faltam 155 dias para seu aniversário</h1>
+		<h1>Faltam <?php echo returnDays(); ?> dias para seu aniversário</h1>
 		<!--LOADING HAMSTER-->
 
 		<div aria-label="Orange and tan hamster running in a metal wheel" role="img" class="wheel-and-hamster">
