96b9f2964d20f86ad86e27d5f18a9e50b848d50d
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-01-31 18:54:47 -0300

    feat(js): new date hour

--- src/js/counter-minutes.js
+++ src/js/counter-minutes.js
@@ -1,4 +1,30 @@
-const html = document.querySelector("#h1");
+/*const html = document.querySelector("#h1");
 
-const data = new Date().getMinutes();
-console.log(data);
+const birthday = "2024-10-01";
+//let calc = minutes - 
+
+function return_date(){
+    const date = new Date();
+    const year = date.getFullYear();
+    const mounth = date.getMonth();
+    const day = date.getDate();
+
+    const hour = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    const fullHour = `${hour}-${minutes}-${seconds}`
+    
+    const fulldate = `${year}-${mounth+1}-${day} ${fullHour}`;
+
+    return fulldate;
+}
+
+setTimeout(()=>{
+    console.log(return_date());
+}, 500);*/
+setTimeout(function(){
+    console.log("Hola Mundo");
+}, 2000);
+
+console.log("setTimeout() Ejemplo...");
