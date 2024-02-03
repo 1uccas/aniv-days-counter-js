74310d36fa97abef7729152621b743875321754b
Lucas N <98057712+Luccxx@users.noreply.github.com>
2024-02-03 18:23:01 -0300

    feat(js): add candle extinguishing and lighting function

---
+++ src/utils/light_candle.js
@@ -0,0 +1,13 @@
+const button = document.querySelector("#button_new_candle");
+const fire = document.querySelectorAll(".fuego");
+
+fire.forEach((fire) =>
+    button.addEventListener('dblclick', ()=>{
+        fire.style.display = "none";
+    })
+);
+fire.forEach((fire) =>
+    button.addEventListener('click', ()=>{
+        fire.style.display = "block";
+    })
+);
